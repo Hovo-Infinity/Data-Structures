443f79b85671c1a7a4ecaa1a2b1346857248779f
HovhannesStepanyan <hovhannes.stepanyan@picsart.com>
2017-07-19 11:12:17 +0400

    Initial Commit

---
+++ Data Structures/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Data Structures
+//
+//  Created by Hovhannes Stepanyan on 7/19/17.
+//  Copyright © 2017 Hovhannes Stepanyan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
